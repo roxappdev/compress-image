48e73758a81cce03d9e533dd2977653c8235f6dd
Aleksandr Gordyushkin <algorq@yandex.ru>
2025-11-03 01:15:26 +0400

    горячие клавиши

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
 chrome.action.onClicked.addListener((tab) => {
   chrome.sidePanel.open({ tabId: tab.id });
 });
+
+// Handle keyboard shortcuts
+chrome.commands.onCommand.addListener((command) => {
+  if (command === '_execute_action') {
+    // Get the active tab and open side panel
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0]) {
+        chrome.sidePanel.open({ tabId: tabs[0].id });
+      }
+    });
+  }
+});
